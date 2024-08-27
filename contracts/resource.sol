8c91b4e2a0b7d20439b4c00f27ae3471418fc53a
PurpleTheBest <vaderofficial@gmail.com>
2024-08-27 19:02:55 +0400

    clean up usings

--- contracts/resource.sol
+++ contracts/resource.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./models.sol";
 
 contract Resource is ERC20, Ownable {
-    using SafeERC20 for IERC20;
 
     struct ResourceDetails {
         string name;
